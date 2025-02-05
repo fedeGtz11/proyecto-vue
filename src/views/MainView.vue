43e7dfa3cfe7757e60bbcad24eca0a61e89864b7
fedeGtz11 <a23311027@uthermosillo.edu.mx>
2025-02-05 13:05:07 -0700

    video 9

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,10 @@
 <template>
-<Header></Header>
+<Header :mensaje="texto"></Header>
 <div class ="container">
     <SideBar></SideBar>
     <div class="content">
+        <input type="texto" v-model="texto">
+        {{ texto }}
         <router-view></router-view>
 
     </div>
@@ -14,6 +16,8 @@
 <script setup>
 import SideBar from '@/components/SideBar.vue';
 import Header from '@/components/Header.vue';
+import { ref } from 'vue';
+const texto = ref('');
 </script>
 
 <style>
