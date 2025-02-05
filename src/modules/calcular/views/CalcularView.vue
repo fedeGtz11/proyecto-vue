02c3bc8689b13312fb83bcca9eed69fb8b644055
fedeGtz11 <a23311027@uthermosillo.edu.mx>
2025-02-05 13:32:16 -0700

    video 11

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,7 +2,9 @@
     <h3>Propiedades Computadas</h3>
     <input type="text" v-model ="valor">
     {{ valorCalculado }}
-    <ResultadoSection :resultado="valorCalculado"></ResultadoSection>
+    <ResultadoSection v-if="mostrarElemento" :resultado="valorCalculado"></ResultadoSection>
+    <div v-else-if="isNaN(valor)">Debe ser un numero para calcular</div>
+    <div v-else>Aqui no se muestra nada</div>
 </template>
 <script setup>
 import {ref, computed} from 'vue';
@@ -11,6 +13,10 @@ const valor = ref('');
 
 const valorCalculado = computed(()=>{
     return valor.value * 3
-})
+});
+
+const mostrarElemento = computed(()=>{
+    return valor.value > 10;
+});
 </script>
 <style scoped></style>
